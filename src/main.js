23310e9dfcfa9399544bb81d4274fc97803cb4e6
Alexey <apple230379@gmail.com>
2021-12-03 20:38:47 +0300

    Added hundlers to trip for open/close edit trip form

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import SortView from './views/sort-view.js';
 import TripListView from './views/trip-list-view.js';
 import NoTripView from './views/no-trip-view.js';
 import TripView from './views/trip-view.js';
-import TripAddView from './views/trip-add-view.js';
+//import TripAddView from './views/trip-add-view.js';
 import TripEditView from './views/trip-edit-view.js';
 import TripInfoView from './views/trip-info-view.js';
 import { generateTrip } from './mock/trip.js';
@@ -27,23 +27,65 @@ render(tripNavigationElement, new TripNavigationView().element, RenderPosition.B
 
 render(tripFiltersElement, new FilterView().element, RenderPosition.BEFOREEND);
 
+const renderTrip = (tripListElement, trip) => {
+  const tripComponent = new TripView(trip);
+  const tripEditComponent = new TripEditView(trip);
+
+  const replaceTripToForm = () => {
+    tripListElement.replaceChild(tripEditComponent.element, tripComponent.element);
+  };
+
+  const replaceFormToTrip = () => {
+    tripListElement.replaceChild(tripComponent.element, tripEditComponent.element);
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToTrip();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
+  tripComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceTripToForm();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  tripEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormToTrip();
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  tripEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceFormToTrip();
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  tripEditComponent.element.querySelector('.event__reset-btn').addEventListener('click', () => {
+    // onClickDeleteEditBtn
+  });
+
+  render(tripListElement, tripComponent.element, RenderPosition.BEFOREEND);
+};
+
+render(tripEventsElement, new TripListView().element, RenderPosition.BEFOREEND);
+
+const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
+
 if (trips.length === 0) {
   render(tripEventsElement, new NoTripView('Everthing').element, RenderPosition.BEFOREEND);
+
+  tripMainElement.querySelector('.trip-main__event-add-btn').addEventListener('click', () => {
+    // onClickNewTripBtn
+  });
 } else {
   render(tripMainElement, new TripInfoView(trips).element, RenderPosition.AFTERBEGIN);
 
-  render(tripEventsElement, new SortView().element, RenderPosition.BEFOREEND);
+  render(tripEventsElement, new SortView().element, RenderPosition.AFTERBEGIN);
 
-  render(tripEventsElement, new TripListView().element, RenderPosition.BEFOREEND);
-
-  const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
-
-  render(tripEventsListElement, new TripAddView(trips[0]).element, RenderPosition.BEFOREEND);
-
-  render(tripEventsListElement, new TripEditView(trips[1]).element, RenderPosition.BEFOREEND);
-
-  for (let i = 2; i < TRIP_COUNT; i++) {
-    render(tripEventsListElement, new TripView(trips[i]).element, RenderPosition.BEFOREEND);
-
+  for (let i = 0; i < TRIP_COUNT; i++) {
+    renderTrip(tripEventsListElement, trips[i]);
   }
 }
