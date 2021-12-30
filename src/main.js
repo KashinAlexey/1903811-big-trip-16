dcbebc5938cb3a3d5a0a54b42ce009bf52641bfa
Alexey <apple230379@gmail.com>
2021-12-30 17:52:09 +0300

    Added navigation functionality

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
-import MainPresenter from './presenter/main-presenter.js';
 import { generateTrip } from '../src/mock/trip.js';
 import { TRIP_COUNT } from './constants.js';
 import DataModel from './model/data-model.js';
 import FilterModel from './model/filter-model.js';
+import StatisticPresenter from './presenter/statistic-presenter.js';
+import TripMainPresenter from './presenter/trip-main-presenter.js';
+import TripEventsPresenter from './presenter/trip-events-presenter.js';
 
 const trips = Array.from({length: TRIP_COUNT}, generateTrip);
 
-const dataModel = new DataModel();
-dataModel.data = trips;
+const tripsModel = new DataModel();
+tripsModel.data = trips;
 
 const filterModel = new FilterModel();
 
-const mainPresenter = new MainPresenter(dataModel, filterModel);
-mainPresenter.init();
+const siteHeaderElement = document.querySelector('.page-header');
+const siteMainElement = document.querySelector('.page-main');
+const tripMainElement = siteHeaderElement.querySelector('.trip-main');
+const tripNavigationElement = tripMainElement.querySelector('.trip-controls__navigation');
+const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
+const tripEventAddBtn = tripMainElement.querySelector('.trip-main__event-add-btn');
+
+const tripMainPresenter = new TripMainPresenter(tripsModel, filterModel, tripMainElement, tripNavigationElement, tripFiltersElement, tripEventAddBtn);
+const tripEventsPresenter = new TripEventsPresenter(tripsModel, filterModel, tripEventsElement);
+const statisticPresenter = new StatisticPresenter(tripsModel, siteMainElement);
+
+tripMainPresenter.init(tripEventsPresenter, statisticPresenter);
--- src/presenter/statistic-presenter.js
+++ src/presenter/statistic-presenter.js
@@ -1,4 +1,24 @@
+import StatsView from '../views/stats-view.js';
+import { RenderPosition } from '../constants.js';
+import { render } from '../utils/render.js';
+import { remove } from '../utils/render.js';
 export default class StatisticPresenter {
+  #tripEventsElement = null;
+  #tripsModel = null;
+  #statsComponent = null;
 
-  init = () => {}
+  constructor (tripsModel, tripEventsElement) {
+    this.#tripsModel = tripsModel;
+    this.#tripEventsElement = tripEventsElement;
+
+    this.#statsComponent = new StatsView();
+  }
+
+  init = () => {
+    render(this.#tripEventsElement, this.#statsComponent, RenderPosition.BEFOREEND);
+  }
+
+  destroy = () => {
+    remove(this.#statsComponent);
+  }
 }
